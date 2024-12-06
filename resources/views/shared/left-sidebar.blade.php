ff7d8cf72d2be72bc72832ae655a6e31cf7536da
Matheus Zoccoler <mtzcooler@gmail.com>
2024-12-06 18:04:08 +0000

    Active route highlighting

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,29 +2,29 @@
     <div class="card-body pt-3">
         <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
             <li class="nav-item">
-                <a class="nav-link text-dark" href="#">
+                <a class="{{ Route::is('dashboard') ? 'text-white bg-primary rounded' : 'text-dark' }} nav-link" href="{{ route('dashboard') }}">
                     <span>Home</span></a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span>Explore</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span>Feed</span></a>
-            </li>
+            </li> --}}
             <li class="nav-item">
-                <a class="nav-link" href="#">
+                <a class="{{ Request::is('terms') ? 'text-white bg-primary rounded' : 'text-dark' }} nav-link" href="{{ url('terms') }}">
                     <span>Terms</span></a>
             </li>
-            <li class="nav-item">
+            {{-- <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span>Support</span></a>
             </li>
             <li class="nav-item">
                 <a class="nav-link" href="#">
                     <span>Settings</span></a>
-            </li>
+            </li> --}}
         </ul>
     </div>
     <div class="card-footer text-center py-2">
--- resources/views/terms.blade.php
+++ resources/views/terms.blade.php
@@ -1,6 +1,21 @@
 @extends('layout.layout')
 @section('content')
 <div class="container py-4">
-Hello!
+    <div class="row">
+        <div class="col-3">
+            @include('shared.left-sidebar')
+        </div>
+        <div class="col-6">
+            <div class="container py-4">
+                Hello!<br/>
+                <br/>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </div>
+        </div>
+        <div class="col-3">
+            @include('shared.search-bar')
+            @include('shared.follow-box')
+        </div>
+    </div>
 </div>
 @endsection
